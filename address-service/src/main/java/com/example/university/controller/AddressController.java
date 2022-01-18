455b4171cc487f376d53020f70908161199c6667
dkkahm <dkkahm@gmail.com>
2022-01-19 07:50:46 +0900

    14. Spring Cloud Config Server / 7. Refresh Properties with Actuator

--- address-service/src/main/java/com/example/university/controller/AddressController.java
+++ address-service/src/main/java/com/example/university/controller/AddressController.java
@@ -5,15 +5,21 @@ import com.example.university.dto.response.AddressResponse;
 import com.example.university.service.AddressService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/address")
+@RefreshScope
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
+
+    @Value("${address.test}")
+    private String test;
 
     @PostMapping("/create")
     public AddressResponse createAddress(@RequestBody CreateAddressRequest createAddressRequest) {
@@ -24,5 +30,10 @@ public class AddressController {
     public AddressResponse getById(@PathVariable long id) {
         log.info("servicing getById");
         return addressService.getById(id);
+    }
+
+    @GetMapping("/test")
+    public String getTest() {
+        return test;
     }
 }
