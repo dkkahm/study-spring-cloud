4531f8f2148c883d38f715a7681ad9bd427044c7
감동기 <dk.kam@tg360tech.com>
2022-01-21 19:02:53 +0900

    6. Microservices with Spring Cloud - V2 / 42. Step 29 - Exploring Rate Limiting and BulkHead Features of Resilience4j - V2

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -1,13 +1,18 @@
 package com.example.currencyconversionservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController
 public class CurrencyConversionController {
 
@@ -28,5 +33,18 @@ public class CurrencyConversionController {
                 from, to, currencyConversion.getConversionMultiple(),
                 quantity, quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment());
+    }
+
+    @GetMapping("/cb-test")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String cbTest() {
+        log.info("Calling cbTest");
+
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:7777/invalid", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Throwable ex) {
+        return "fallback-response";
     }
 }
